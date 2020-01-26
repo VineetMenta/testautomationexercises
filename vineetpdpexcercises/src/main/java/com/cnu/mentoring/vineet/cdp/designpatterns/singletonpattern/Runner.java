8aae5066c86c08da1746cf4f43e15e18a1c1d53d
VineetMenta <vineetmenta1998@gmail.com>
2020-01-26 09:43:48 +0530

    commiting code to break singleton pattern

--- vineetpdpexcercises/src/main/java/com/cnu/mentoring/vineet/cdp/designpatterns/singletonpattern/Runner.java
+++ vineetpdpexcercises/src/main/java/com/cnu/mentoring/vineet/cdp/designpatterns/singletonpattern/Runner.java
@@ -2,6 +2,8 @@ package com.cnu.mentoring.vineet.cdp.designpatterns.singletonpattern;
 
 import org.apache.log4j.Logger;
 
+import java.lang.reflect.Constructor;
+
 public class Runner {
 
     private static final Logger LOGGER = Logger.getLogger(Runner.class);
@@ -19,5 +21,20 @@ public class Runner {
         ThreadSafeSingleton threadOne = ThreadSafeSingleton.getInstanceOfSingleton();
         ThreadSafeSingleton threadTwo = ThreadSafeSingleton.getInstanceOfSingleton();
         LOGGER.info(threadOne.hashCode() + " " + threadTwo.hashCode());
+        breakingSingletonPattern();
+    }
+
+    public static void breakingSingletonPattern(){
+        LazyInitializationSingleTon instanceOne = LazyInitializationSingleTon.getInstanceOfSingleton();
+        LazyInitializationSingleTon instanceTwo = null;
+        try{
+            Constructor constructor = LazyInitializationSingleTon.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            instanceTwo = (LazyInitializationSingleTon) constructor.newInstance();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        LOGGER.info("Breaking the Singleton");
+        LOGGER.info(instanceOne.hashCode() + " " + instanceTwo.hashCode());
     }
 }
