bd282513c1f3737f0bd0d7bf089238606246db3b
VineetMenta <vineetmenta1998@gmail.com>
2020-02-04 19:20:12 +0530

    Added java 8 Hometask 1

---
+++ vineetpdpexcercises/src/main/java/com/cnu/mentoring/vineet/cdp/java8/HomeTask_1/CheckString.java
@@ -0,0 +1,6 @@
+package com.cnu.mentoring.vineet.cdp.java8.HomeTask_1;
+
+@FunctionalInterface
+public interface CheckString {
+    boolean checkString(String string);
+}
---
+++ vineetpdpexcercises/src/main/java/com/cnu/mentoring/vineet/cdp/java8/HomeTask_1/Palindrome.java
@@ -0,0 +1,9 @@
+package com.cnu.mentoring.vineet.cdp.java8.HomeTask_1;
+
+public class Palindrome implements CheckString {
+    @Override
+    public boolean checkString(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string).reverse();
+        return stringBuilder.toString().equalsIgnoreCase(string);
+    }
+}
