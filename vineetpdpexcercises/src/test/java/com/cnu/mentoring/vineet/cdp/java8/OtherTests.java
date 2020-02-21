1ff6e359cef13a9041e026064ba086c2b7c497e7
VineetMenta <vineetmenta1998@gmail.com>
2020-02-21 10:53:57 +0530

    Adding other Java 8 Tasks

---
+++ vineetpdpexcercises/src/test/java/com/cnu/mentoring/vineet/cdp/java8/OtherTests.java
@@ -0,0 +1,53 @@
+package com.cnu.mentoring.vineet.cdp.java8;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class OtherTests {
+
+    private static final Logger LOGGER = Logger.getLogger(OtherTests.class);
+
+    TreeSet<Integer> actualIntegers;
+    TreeSet<Integer> reverseSortedIntegersOldWay;
+    TreeSet<Integer> reverseSortedIntegersJavaEight;
+
+    @BeforeClass
+    public void initialize(){
+        actualIntegers = new TreeSet<>();
+        actualIntegers.add(5);
+        actualIntegers.add(1);
+        actualIntegers.add(6);
+        actualIntegers.add(2);
+        actualIntegers.add(4);
+        actualIntegers.add(3);
+    }
+
+    @Test
+    public void testTreeMapSortOldWay(){
+        Comparator<Integer> comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1.compareTo(o2);
+            }
+        };
+        reverseSortedIntegersOldWay = new TreeSet<>(comparator.reversed());
+        reverseSortedIntegersOldWay.addAll(actualIntegers);
+        LOGGER.info(actualIntegers);
+        LOGGER.info(reverseSortedIntegersOldWay);
+        Assert.assertNotSame(actualIntegers, reverseSortedIntegersOldWay);
+    }
+
+    @Test
+    public void testTreeMapSortJavaEight(){
+        reverseSortedIntegersJavaEight = new TreeSet<>(Comparator.reverseOrder());
+        reverseSortedIntegersJavaEight.addAll(actualIntegers);
+        LOGGER.info(actualIntegers);
+        LOGGER.info(reverseSortedIntegersJavaEight);
+        Assert.assertNotSame(actualIntegers, reverseSortedIntegersJavaEight);
+    }
+}
